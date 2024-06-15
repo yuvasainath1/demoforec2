40fad447b0a7b03ec9e32e3b7618b74c66287da8
Sainath <yuvasainath85@gmail.com>
2024-06-16 01:11:48 +0530

    dkjk

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Docker!nkjvnrkjvnkrjvn');
+  res.send('Sadhinchav ra BUlloda');
 });
 
 app.listen(port, () => {
